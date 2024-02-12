08238e1aa11bcb879a0c0d33ec9346326651a4d2
Juan Molina Riddell <j.molina.riddell@gmail.com>
2024-02-12 12:26:20 -0300

    ver: increase minor version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="validpeas",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     description="Tools for structured validations",
     long_description=open("README.md").read(),
